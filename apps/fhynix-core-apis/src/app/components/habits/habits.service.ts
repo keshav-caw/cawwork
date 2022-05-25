1157b762f418e6e394aa32bfd99d549822973ab6
Alekhya Gopu <alekhya.gopu@cawstudios.com>
2022-05-25 17:17:42 +0530

    fix the habits api and validation for family member issue

--- apps/fhynix-core-apis/src/app/components/habits/habits.service.ts
+++ apps/fhynix-core-apis/src/app/components/habits/habits.service.ts
@@ -1,5 +1,7 @@
+import { ApiErrorCode } from 'apps/shared/payloads/error-codes'
 import { inject, injectable } from 'inversify'
 import 'reflect-metadata'
+import { ArgumentValidationError } from '../../common/errors/custom-errors/argument-validation.error'
 import { HabitsServiceInterface } from '../../common/interfaces/habits-service.interface'
 import { FamilyMemberHabitsModel } from '../../common/models/family-member-habits-model'
 import { HabitsModel } from '../../common/models/habits-model'
@@ -22,6 +24,13 @@ export class HabitsService implements HabitsServiceInterface {
   async createHabitsForRelationship(
     relationshipHabits: FamilyMemberHabitsModel[],
   ): Promise<FamilyMemberHabitsModel[]> {
+    if (relationshipHabits?.length < 2) {
+      throw new ArgumentValidationError(
+        'Atleast 2 habits must be added',
+        relationshipHabits,
+        ApiErrorCode.E0021,
+      )
+    }
     let familyMemberIds = relationshipHabits.map(
       (relationshipHabit) => relationshipHabit.familyMemberId,
     )
