20e1eddce9d326b8af4f5db0770380b6a638fcd5
keshav-dev <keshavsinghal201920@gmail.com>
2022-06-10 21:01:55 +0530

    Fixing before DB query functionality

--- apps/fhynix-core-apis/src/app/common/data/datastore.ts
+++ apps/fhynix-core-apis/src/app/common/data/datastore.ts
@@ -21,16 +21,18 @@ export class DataStore {
 
     this.dbClient.$use(async (params, next) => {
       const userId = this.requestContext.getUserId()
-      if(params.action=='create' || params.action=='update'){
-        params.args.data.updatedBy =userId;
-      }
-      if(params.action=='create'){
-        params.args.data.createdBy = userId;
-      }
+      
       if(params.action=='upsert'){
         params.args.update.updatedBy = userId;
         params.args.create.updatedBy = userId;
         params.args.create.createdBy = userId;
+      }
+      else if(params.action=='create'){
+        params.args.data.updatedBy =userId;
+        params.args.data.createdBy = userId;
+      }
+      else if(params.action=='update'){
+        params.args.data.updatedBy =userId;
       }
       
       const result = await next(params)
